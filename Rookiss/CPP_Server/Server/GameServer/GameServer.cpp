1bc498dbef37726a0cdb293c3107efb4fcef08b3
bbdlek <bbdlek@gmail.com>
2023-07-31 00:44:34 +0900

    1-2. 쓰레드 생성
    1-2. 쓰레드 생성

--- Rookiss/CPP_Server/Server/GameServer/GameServer.cpp
+++ Rookiss/CPP_Server/Server/GameServer/GameServer.cpp
@@ -1,7 +1,34 @@
 ﻿#include "pch.h"
 #include <iostream>
 
+#include <thread>
+
+void HelloThread()
+{
+    cout << "Hello Thread" << endl;
+}
+
+void HelloThread_2(int32 num)
+{
+    cout << num << endl;
+}
+
 int main()
 {
-    HelloWorld();
+
+    vector<std::thread> v;
+
+    for (int32 i = 0; i < 10; i++)
+    {
+        v.push_back(std::thread(HelloThread_2, i));
+    }
+
+    for (int32 i = 0; i < 10; i++)
+    {
+        if (v[i].joinable())
+            v[i].join();
+    }
+
+    cout << "Hello Main" << endl;
+
 }
