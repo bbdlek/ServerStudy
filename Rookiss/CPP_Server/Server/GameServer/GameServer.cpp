7d48aa7f882572452cff66581f8a1b3caeb8de51
bbdlek <bbdlek@gmail.com>
2023-08-01 00:28:52 +0900

    1-3. Atomic
    1-3. Atomic

--- Rookiss/CPP_Server/Server/GameServer/GameServer.cpp
+++ Rookiss/CPP_Server/Server/GameServer/GameServer.cpp
@@ -2,33 +2,45 @@
 #include <iostream>
 
 #include <thread>
+#include <atomic>
 
-void HelloThread()
+// atomic : All-Or-Nothing
+atomic<int32> sum = 0; // Heap(전역)
+
+void Add()
 {
-    cout << "Hello Thread" << endl;
+	//int32 sum = 0 // Stack
+	for (int32 i = 0; i < 100'0000; i++)
+	{
+		sum.fetch_add(1);
+		//sum++; // 사용해도 되나 atomic인지 보기 편하게
+		/*int32 eax = sum;
+		eax = eax + 1;
+		sum = eax;*/
+	}
 }
 
-void HelloThread_2(int32 num)
+void Sub()
 {
-    cout << num << endl;
+	for (int32 i = 0; i < 100'0000; i++)
+	{
+		sum.fetch_add(-1);
+		//sum--;
+		/*int32 eax = sum;
+		eax = eax - 1;
+		sum = eax;*/
+	}
 }
 
 int main()
 {
+	Add();
+	Sub();
+	cout << sum << endl;
 
-    vector<std::thread> v;
-
-    for (int32 i = 0; i < 10; i++)
-    {
-        v.push_back(std::thread(HelloThread_2, i));
-    }
-
-    for (int32 i = 0; i < 10; i++)
-    {
-        if (v[i].joinable())
-            v[i].join();
-    }
-
-    cout << "Hello Main" << endl;
-
+	std::thread t1(Add);
+	std::thread t2(Sub);
+	t1.join();
+	t2.join();
+	cout << sum << endl;
 }
