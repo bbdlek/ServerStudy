296c26a5a67bb574d190e311fed5a54258b60037
bbdlek <bbdlek@gmail.com>
2023-08-01 00:49:48 +0900

    1-4. Lock 기초
    1-4. Lock 기초

--- Rookiss/CPP_Server/Server/GameServer/GameServer.cpp
+++ Rookiss/CPP_Server/Server/GameServer/GameServer.cpp
@@ -3,44 +3,72 @@
 
 #include <thread>
 #include <atomic>
+#include <mutex>
 
-// atomic : All-Or-Nothing
-atomic<int32> sum = 0; // Heap(전역)
+// [1][2][3] -> delete가 중복으로 실행되면서 삭제 못함
+// [1][2][3][][][][][][][][][] -> reserve 했어도 같은 곳에 push_back 할 수도 있음
 
-void Add()
+vector<int32> v;
+
+// Mutual Exclusive (상호배타적)
+mutex m; // 일종의 화장실 자물쇠
+
+// RAII (Resource Acquisition Is Initialization)
+// 생성자에서 잠구고 소멸자에서 풀기
+template<typename T>
+class LockGuard
 {
-	//int32 sum = 0 // Stack
-	for (int32 i = 0; i < 100'0000; i++)
+public:
+	LockGuard(T& m)
 	{
-		sum.fetch_add(1);
-		//sum++; // 사용해도 되나 atomic인지 보기 편하게
-		/*int32 eax = sum;
-		eax = eax + 1;
-		sum = eax;*/
+		_mutex = &m;
+		_mutex->lock();
 	}
-}
 
-void Sub()
-{
-	for (int32 i = 0; i < 100'0000; i++)
+	~LockGuard()
 	{
-		sum.fetch_add(-1);
-		//sum--;
-		/*int32 eax = sum;
-		eax = eax - 1;
-		sum = eax;*/
+		_mutex->unlock();
+	}
+
+private:
+	T* _mutex;
+};
+
+void Push()
+{
+	for (int32 i = 0; i < 10000; i++)
+	{
+		// 자물쇠 잠구기
+		//LockGuard<std::mutex> lockGuard(m);
+		std::lock_guard<std::mutex> lockGuard(m);
+		//std::unique_lock<std::mutex> uniqueLock(m, std::defer_lock); // 나중에 잠구기 가능
+
+		//uniqueLock.lock();
+		//m.lock();
+		//m.lock();
+
+		v.push_back(i);
+
+		if (i == 5000)
+		{
+			//m.unlock();
+			break;
+		}
+
+		// 자물쇠 풀기
+		//m.unlock();
+		//m.unlock();
 	}
 }
 
 int main()
 {
-	Add();
-	Sub();
-	cout << sum << endl;
+	std::thread t1(Push);
+	std::thread t2(Push);
 
-	std::thread t1(Add);
-	std::thread t2(Sub);
 	t1.join();
 	t2.join();
-	cout << sum << endl;
+
+	cout << v.size() << endl;
+
 }
